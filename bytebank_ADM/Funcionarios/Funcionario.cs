39a6d20dd4af3f49533847f424ef56a51c814077
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-09-27 23:35:16 -0300

    Usando o this

--- bytebank_ADM/Funcionarios/Funcionario.cs
+++ bytebank_ADM/Funcionarios/Funcionario.cs
@@ -8,13 +8,30 @@ namespace bytebank_ADM.Funcionarios
 {
     public class Funcionario
     {
+        // 0 - Funcionário
+        // 1 - Diretor
+        // 2 - Designer
+        // N - ...
+        private int _tipo;
         public string Nome { get; set; }
         public string Cpf { get; set; }
         public double Salario { get; set; }
 
+        public Funcionario(int _tipo)
+        {
+            this._tipo = _tipo;
+        }
+
         public double getBonificacao()
         {
-            return Salario * 0.1;
+            if (_tipo == 1)
+            {
+                return Salario;
+            }
+            else
+            {
+                return Salario * 0.1;
+            }
         }
     }
 }
--- bytebank_ADM/Program.cs
+++ bytebank_ADM/Program.cs
@@ -2,9 +2,14 @@
 
 Console.WriteLine("Boas Vindas, ao ByteBank Administração.");
 
-Funcionario pedro = new Funcionario();
+Funcionario pedro = new Funcionario(1);
 pedro.Nome = "João";
 pedro.Cpf = "12345698";
 pedro.Salario = 2000;
 
+Console.WriteLine("Nome: " + pedro.Nome);
+Console.WriteLine("CPF: " + pedro.Cpf);
+Console.WriteLine("Salário: " + pedro.Salario);
 Console.WriteLine("Bonificação: " + pedro.getBonificacao());
+
+Console.ReadKey();
