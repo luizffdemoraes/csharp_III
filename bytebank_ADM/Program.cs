586c1eb1f548f2a511934ed52071c08c80bf9764
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-09-28 00:05:40 -0300

    Total de bonificações

--- bytebank_ADM/Program.cs
+++ bytebank_ADM/Program.cs
@@ -1,15 +1,18 @@
 ﻿using bytebank_ADM.Funcionarios;
+using bytebank_ADM.Utilitario;
 
 Console.WriteLine("Boas Vindas, ao ByteBank Administração.");
+
+GerenciadorDeBonificacao gerenciador = new GerenciadorDeBonificacao();
 
 Funcionario pedro = new Funcionario();
 pedro.Nome = "Pedro";
 pedro.Cpf = "12345698";
 pedro.Salario = 2000;
 
-Console.WriteLine("Nome: " + pedro.Nome);
-Console.WriteLine("CPF: " + pedro.Cpf);
-Console.WriteLine("Salário: " + pedro.Salario);
+//Console.WriteLine("Nome: " + pedro.Nome);
+//Console.WriteLine("CPF: " + pedro.Cpf);
+//Console.WriteLine("Salário: " + pedro.Salario);
 
 Diretor paula = new Diretor();
 paula.Nome = "Paula";
@@ -18,5 +21,10 @@ paula.Salario = 5000;
 
 Console.WriteLine("Bonificação: " + pedro.getBonificacao());
 Console.WriteLine("Bonificação: " + paula.getBonificacao());
+
+gerenciador.Registrar(pedro);
+gerenciador.Registrar(paula);
+
+Console.WriteLine("Total de Bonificacao:" + gerenciador.getBonificacao());
 
 Console.ReadKey();
