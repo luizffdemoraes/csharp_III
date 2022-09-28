59fcd0dca708eb9eefd3222e715ba8dd46247760
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-09-27 23:17:37 -0300

    Organizando o projeto

---
+++ bytebank_ADM/Program.cs
@@ -0,0 +1,10 @@
+﻿using bytebank_ADM.Funcionarios;
+
+Console.WriteLine("Boas Vindas, ao ByteBank Administração.");
+
+Funcionario pedro = new Funcionario();
+pedro.Nome = "João";
+pedro.Cpf = "12345698";
+pedro.Salario = 2000;
+
+Console.WriteLine("Bonificação: " + pedro.getBonificacao());
