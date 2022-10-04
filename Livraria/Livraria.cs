ab5dbea73a0891a19165150c4757779cc81636ba
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-10-03 22:56:35 -0300

    Desafio: personalizando um construtor

---
+++ Livraria/Livraria.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livraria
+{
+    public class Livraria
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Isbn { get; set; } 
+        public int AnoDePublicacao { get; set; }
+        public string EstiloLiterario { get; set; }
+        public int NumeroDePaginas { get; set; }
+        public string PaisDePublicacao { get; set; }
+
+        public Livraria(int Isbn, string Titulo)
+        {
+            this.Isbn = Isbn;
+            this.Titulo = Titulo;
+        }
+
+
+ 
+
+
+
+    }
+}
